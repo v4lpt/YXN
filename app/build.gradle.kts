6b32c837668d125ba9652c8ee6a5da397052eabd
v4lpt <v4lpt@posteo.net>
2024-08-08 09:04:46 +0200

    updated icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "v4lpt.vpt.f006.yxn"
         minSdk = 24
         targetSdk = 34
-        versionCode = 101
-        versionName = "1.0.1"
+        versionCode = 102
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
